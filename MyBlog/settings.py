80c5dabda3bb7f1a1826079d7cdaee439efbe341
ahmedeid <kingmedo954@gmail.com>
2020-09-15 14:39:48 +0200

    adding sending mail feature

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -125,3 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'ahmedeid6422@gmail.com'
+EMAIL_HOST_PASSWORD = 'ahmedeid0117'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True

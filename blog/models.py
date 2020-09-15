5e6b6db28b440d6b95f9e3fb4e0aaab8142bfeeb
ahmedeid <kingmedo954@gmail.com>
2020-09-15 10:46:29 +0200

    adding the urls to the view and adding a Canonical url

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,12 @@
 import django
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 # Creating a customized manager
+
+
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super(PublishedManager, self).get_queryset().filter(status='published')
@@ -26,6 +29,15 @@ class Post(models.Model):
 
     class Meta:
         ordering = ('-publish',)
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[
+                           self.publish.year,
+                           self.publish.month,
+                           self.publish.day,
+                           self.slug
+                       ])
 
     def __str__(self):
         return self.title

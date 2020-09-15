21c914d59587f151b2ac48f6db020d84128b9609
ahmedeid <kingmedo954@gmail.com>
2020-09-15 18:11:15 +0200

    recommending similar posts based on the tags

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,8 @@
-from django.http import HttpResponse
-from django.views.generic import ListView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from django.core.mail import send_mail
 from taggit.models import Tag
+from django.db.models import Count
 
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
@@ -52,9 +51,15 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
+    post_tags_id = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_id).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                        .order_by('-same_tags', '-publish')[:4]
+
     return render(request, 'blog/post/detail.html',
                   {'post': post, 'comments': comments,
-                   'new_comment': new_comment, 'comment_from': comment_form})
+                   'new_comment': new_comment, 'comment_from': comment_form,
+                   'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):

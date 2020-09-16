7fa4bfc70f023db81c3830cc4344692c129100f6
ahmedeid <kingmedo954@gmail.com>
2020-09-16 19:24:15 +0200

    searching with trigram(pg_trgm) similarity

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.core.mail import send_mail
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import TrigramSimilarity
 
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm, SearchForm
@@ -94,13 +94,9 @@ def post_search(request):
         if form.is_valid():
             query = form.cleaned_data['query']
 
-            search_vector = SearchVector('title', 'body')
-            search_query = SearchQuery(query)
-
             results = Post.published.annotate(
-                search=search_vector,
-                rank=SearchRank(search_vector, search_query)
-            ).filter(search=search_query).order_by('-rank')
+                similarity=TrigramSimilarity('title', query)
+            ).filter(similarity__gt=0.3).order_by('-similarity')
 
     return render(request, 'blog/post/search.html',
                   {'form': form, 'query': query, 'results': results})

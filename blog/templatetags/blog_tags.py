2f5f6c2c480dc486a288916a72c0c8ed7fc7957d
ahmedeid <kingmedo954@gmail.com>
2020-09-16 16:57:04 +0200

    adding the ability to use markdown in the post body

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,10 +1,20 @@
 from django import template
 from django.db.models import Count
+from django.utils.safestring import mark_safe
+import markdown
+
 from ..models import Post
 
 register = template.Library()
 
 
+# filters
+@register.filter(name="markdown")
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
+
+
+# simple tags
 @register.simple_tag
 def total_posts():
     return Post.published.count()
@@ -17,6 +27,7 @@ def get_most_commented_posts(count=5):
     ).order_by("-total_comments")[:count]
 
 
+# inclusion tags
 @register.inclusion_tag('blog/post/latest_posts.html')
 def show_latest_posts(count=5):
     latest_posts = Post.published.order_by('-publish')[:count]
